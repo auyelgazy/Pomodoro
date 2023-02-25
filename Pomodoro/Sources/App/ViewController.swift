c618ba98bb160cc60181a81f212585331c9c7a01
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-02-25 13:21:09 +0600

    Initial commit.

---
+++ Pomodoro/Sources/App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by Kuanysh al-Khattab Auyelgazy on 25.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
